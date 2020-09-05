432da857416f1b97e3bb8f22beed7475342b985b
ygy <2249312343@qq.com>
2020-09-05 10:30:41 +0800

    安装了nprogress

--- src/http/index.js
+++ src/http/index.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
-// import Vue from 'vue'
-// Vue.prototype.$axios = axios;
-
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 // axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('token') 第一次不行
 
 //请求拦截
 axios.interceptors.request.use((config)=>{
+    NProgress.start();
     config.headers.Authorization =  sessionStorage.getItem('token');//添加token
     return config;
 })
 
 // // 响应拦截
-// axios.interceptors.response.use(config=>{
-
-//     return config;
-// })
+axios.interceptors.response.use(config=>{
+    NProgress.done();
+    return config;
+})
 
 
 export default axios;
